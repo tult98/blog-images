1490ffb446e3eee1787f2b70ccff540847cf80e4
Tu Thanh Le <tu.le@politetech.com>
2023-12-07 22:45:40 +0700

    fix: make it work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
-// import { HttpModule } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { ScheduleModule } from '@nestjs/schedule';
-// import { ServeStaticModule } from '@nestjs/serve-static';
-// import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { TasksService } from 'src/task.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    // ServeStaticModule.forRoot({
-    //   rootPath: join(__dirname, '..', 'public'),
-    // }),
-    // ConfigModule.forRoot(),
-    // ScheduleModule.forRoot(),
-    // HttpModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    HttpModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
